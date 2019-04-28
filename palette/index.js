cea12ba543ab76250d66941a9faee826858db0cd
juliastetskaya <juliastetskaya@gmail.com>
2019-04-28 20:35:06 +0300

    feat: implement choose color tool

--- palette/index.js
+++ palette/index.js
@@ -1,8 +1,22 @@
+const rgbToHex = (color) => {
+  const arr = color.split('(')[1].split(')')[0].split(',');
+  const arrHex = arr.map((x) => {
+    const num = parseInt(x, 10).toString(16);
+    return (num.length === 1) ? `0${num}` : num;
+  });
+  return `#${arrHex.join('')}`;
+};
+
 const state = {
   currentTool: '',
+  currentColor: '#00eeee',
+  previousColor: '#00ff00',
 };
 
 const palette = document.querySelector('.palette__wrapper');
+const canvasList = document.querySelector('.canvas__list');
+const currentColorElem = document.querySelector('.current-color');
+const previuosColorElem = document.querySelector('.previous-color');
 
 palette.addEventListener('click', (event) => {
   let { target } = event;
@@ -22,19 +36,28 @@ palette.addEventListener('click', (event) => {
   }
 });
 
-const currentColorElem = document.querySelector('.current-color');
-let currentColor = currentColorElem.value;
-const previuosColorElem = document.querySelector('.previous-color');
-
-
-currentColorElem.addEventListener('change', () => {
-  if (currentColorElem.value !== currentColor) {
-    previuosColorElem.style.backgroundColor = currentColor;
-    currentColor = currentColorElem.value;
+document.addEventListener('click', (event) => {
+  const { target } = event;
+  if (!target.classList.contains('palette__button--color') && state.currentTool === 'color') {
+    const { backgroundColor } = getComputedStyle(target);
+    console.dir(target);
+    console.log(backgroundColor);
+    currentColorElem.value = rgbToHex(backgroundColor);
+    previuosColorElem.style.backgroundColor = state.currentColor;
+    state.previousColor = state.currentColor;
+    state.currentColor = currentColorElem.value;
   }
 });
 
-const canvasList = document.querySelector('.canvas__list');
+currentColorElem.addEventListener('change', () => {
+  console.log('!!!!!!!!!!!!!!!');
+  if (currentColorElem.value !== state.currentColor) {
+    previuosColorElem.style.backgroundColor = state.currentColor;
+    state.previousColor = state.currentColor;
+    state.currentColor = currentColorElem.value;
+  }
+});
+
 
 canvasList.addEventListener('click', (event) => {
   let { target } = event;
@@ -46,7 +69,7 @@ canvasList.addEventListener('click', (event) => {
         target.classList.toggle('circle');
       }
       if (currentTool === 'paint') {
-        target.style.backgroundColor = currentColor;
+        target.style.backgroundColor = state.currentColor;
       }
     }
     target = target.parentNode;
