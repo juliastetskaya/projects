3afe1f7398289805146075873cafa7b2d452dd2e
juliastetskaya <juliastetskaya@gmail.com>
2019-06-02 23:06:35 +0300

    feat: add speed and fullscreen mode for animation

--- animation-player/src/App.js
+++ animation-player/src/App.js
@@ -3,34 +3,76 @@ import createElement from './lib';
 export default class App {
   constructor() {
     this.text = 'Your browser is not supported canvas!';
-    this.frames = [];
+    this.speed = 10;
+  }
+
+  createButtonsForFrame() {
+    const buttonDelete = createElement('button', 'button button__delete');
+    const buttonCopy = createElement('button', 'button button__copy');
+    const buttonMove = createElement('button', 'button button__move');
+
+    const buttons = [buttonDelete, buttonCopy, buttonMove];
+
+    buttonCopy.addEventListener('click', this.copyFrame.bind(this));
+    buttonDelete.addEventListener('click', this.deleteFrame.bind(this));
+
+    buttons.forEach(button => button.setAttribute('type', 'button'));
+
+    return buttons;
   }
 
   createFrame() {
     const frames = document.querySelector('.list__frames');
     const counter = frames ? frames.childNodes.length + 1 : 1;
 
-    const buttonDelete = createElement('button', 'button button__delete');
-    const buttonCopy = createElement('button', 'button button__copy');
-    const buttonMove = createElement('button', 'button button__move');
+    const buttons = this.createButtonsForFrame();
+
     const number = createElement('div', 'number__frame', `${counter}`);
-    const buttons = [buttonDelete, buttonCopy, buttonMove];
-    buttons.forEach(button => button.setAttribute('type', 'button'));
 
     const activeFrame = document.querySelector('.canvas--active');
     if (activeFrame) {
       activeFrame.classList.remove('canvas--active');
     }
 
-    const frameCanvas = createElement('canvas', `frame__canvas frame__canvas-${counter} canvas--active`);
+    const frameCanvas = createElement('canvas', 'frame__canvas canvas--active');
     frameCanvas.width = 200;
     frameCanvas.height = 200;
 
-    buttonCopy.addEventListener('click', this.copyFrame.bind(this));
     const frame = createElement('li', 'frames__item', frameCanvas, number, ...buttons);
     frames.append(frame);
 
+    if (counter > 1) {
+      frames.firstChild.querySelector('.button__delete').style.display = '';
+      frames.firstChild.querySelector('.button__move').style.display = '';
+    } else {
+      frames.firstChild.querySelector('.button__delete').style.display = 'none';
+      frames.firstChild.querySelector('.button__move').style.display = 'none';
+    }
+
     return frame;
+  }
+
+  deleteFrame(event) {
+    const frame = event.target.closest('.frames__item');
+    document.querySelector('.list__frames').removeChild(frame);
+
+    const canvas = document.querySelector('.canvas');
+    const cxt = canvas.getContext('2d');
+
+    cxt.clearRect(0, 0, canvas.width, canvas.height);
+
+    const frames = document.querySelector('.list__frames');
+    if (frames.children.length === 1) {
+      frames.firstChild.querySelector('.button__delete').style.display = 'none';
+      frames.firstChild.querySelector('.button__move').style.display = 'none';
+    }
+
+    if (frame.firstChild.matches('.canvas--active')) {
+      frames.lastChild.firstChild.classList.add('canvas--active');
+    }
+
+    this.updateFramesNumbers();
+    this.setFrame();
   }
 
   copyMainCanvas() {
@@ -67,8 +109,16 @@ export default class App {
 
     this.copyMainCanvas();
 
-    // const canvas = document.querySelector('.canvas');
     ctx.drawImage(sourceCanvas, 0, 0);
+    this.setFrame();
+  }
+
+  updateFramesNumbers() {
+    const frames = [...document.querySelector('.list__frames').children];
+    frames.forEach((frame, index) => {
+      const div = frame.querySelector('div');
+      div.innerHTML = index + 1;
+    });
   }
 
   start() {
@@ -78,10 +128,12 @@ export default class App {
 
     const input = createElement('input');
     input.setAttribute('type', 'color');
+    input.setAttribute('value', '#00ff00');
 
     const label = createElement('label', 'label', 'Color: ', input);
 
     const frames = createElement('ul', 'list__frames');
+    frames.addEventListener('click', this.changeActiveFrame.bind(this));
 
     const button = createElement('button', 'button__new-frame', 'Add new frame');
     button.addEventListener('click', this.createNewFrame.bind(this));
@@ -92,12 +144,41 @@ export default class App {
     animation.width = 300;
     animation.height = 300;
 
-    document.body.append(label, framesWrapper, canvas, animation);
+    const buttonFS = createElement('button', 'button-fullscreen', 'Fullscreen');
+    buttonFS.setAttribute('type', 'button');
+    buttonFS.addEventListener('click', () => {
+      const canv = document.querySelector('.canvas__animation');
+      canv.requestFullscreen();
+    });
+
+    const inputRange = createElement('input', 'animation__speed');
+    inputRange.setAttribute('type', 'range');
+    inputRange.setAttribute('name', 'speed');
+    inputRange.setAttribute('value', '10');
+    inputRange.setAttribute('min', '0');
+    inputRange.setAttribute('max', '24');
+
+    const span = createElement('span', 'speed', `${this.speed} FPS`);
+
+    const labelAnimation = createElement('label', 'animation__label', 'Animation speed: ', span, inputRange);
+
+    const animationWrapper = createElement('div', 'animation__wrapper', animation, buttonFS, labelAnimation);
+
+    document.body.append(label, framesWrapper, canvas, animationWrapper);
 
     this.createFrame();
 
     this.draw();
     this.startAnimation();
+  }
+
+  changeActiveFrame(event) {
+    if (event.target.tagName.toLowerCase() === 'canvas') {
+      const activeCanvas = document.querySelector('.canvas--active');
+      activeCanvas.classList.remove('canvas--active');
+      event.target.classList.add('canvas--active');
+      this.setFrame();
+    }
   }
 
   draw() {
@@ -150,16 +231,43 @@ export default class App {
     ctx.drawImage(image, 0, 0, 500, 500, 0, 0, 200, 200);
   }
 
+  setFrame() {
+    const sourseCanvas = document.querySelector('.canvas--active');
+
+    const canvas = document.querySelector('.canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.drawImage(sourseCanvas, 0, 0, 200, 200, 0, 0, 500, 500);
+  }
+
   startAnimation() {
     let count = 0;
+    let timer;
     const animation = document.querySelector('.canvas__animation');
     const ctx = animation.getContext('2d');
 
-    setInterval(() => {
-      const frames = [...document.querySelector('.list__frames').children];
-      ctx.clearRect(0, 0, 300, 300);
-      ctx.drawImage(frames[count].firstChild, 0, 0, 200, 200, 0, 0, 300, 300);
-      count = count === frames.length - 1 ? 0 : count + 1;
-    }, 1000 / 5);
+    const start = () => {
+      if (this.speed > 0) {
+        const frames = [...document.querySelector('.list__frames').children];
+        ctx.clearRect(0, 0, animation.width, animation.height);
+        const image = frames[count % frames.length].firstChild;
+        ctx.drawImage(image, 0, 0, 200, 200, 0, 0, 300, 300);
+        count += 1;
+      }
+    };
+
+    timer = setInterval(() => start(), 1000 / Number(this.speed));
+
+    const inputRange = document.querySelector('.animation__speed');
+    inputRange.addEventListener('input', () => {
+      this.speed = inputRange.value;
+      clearInterval(timer);
+      timer = setInterval(() => start(), 1000 / Number(this.speed));
+
+      const labelAnimation = document.querySelector('.speed');
+      console.dir(labelAnimation);
+      labelAnimation.innerHTML = `${this.speed} FRS`;
+    });
   }
 }
