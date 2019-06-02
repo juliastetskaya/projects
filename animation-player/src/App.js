3891b45d85593970eba2b1545b5c98c66b6cf5ec
juliastetskaya <juliastetskaya@gmail.com>
2019-06-02 10:26:29 +0300

    feat: add frames and canvas for animation

--- animation-player/src/App.js
+++ animation-player/src/App.js
@@ -5,9 +5,61 @@ export default class App {
     this.text = 'Your browser is not supported canvas!';
   }
 
+  static createFrame(counter) {
+    const buttonDelete = createElement('button', 'button button__delete');
+    const buttonCopy = createElement('button', 'button button__copy');
+    const buttonMove = createElement('button', 'button button__move');
+    const number = createElement('div', 'number__frame', `${counter}`);
+    const buttons = [buttonDelete, buttonCopy, buttonMove];
+    buttons.forEach(button => button.setAttribute('type', 'button'));
+
+    const activeFrame = document.querySelector('.canvas--active');
+    if (activeFrame) {
+      activeFrame.classList.remove('canvas--active');
+    }
+
+    const frameCanvas = createElement('canvas', `frame__canvas frame__canvas-${counter} canvas--active`);
+    frameCanvas.width = 200;
+    frameCanvas.height = 200;
+
+    buttonCopy.addEventListener('click', App.copyFrame);
+    const frame = createElement('li', 'frames__item', frameCanvas, number, ...buttons);
+
+    return frame;
+  }
+
+  static copyMainCanvas() {
+    const canvas = document.querySelector('.canvas');
+    const newCanvas = createElement('canvas', 'canvas');
+    newCanvas.width = 500;
+    newCanvas.height = 500;
+
+    const ctx = newCanvas.getContext('2d');
+    ctx.drawImage(canvas, 0, 0);
+
+    document.body.replaceChild(newCanvas, canvas);
+    App.draw();
+  }
+
+  static copyFrame() {
+    const frames = document.querySelector('.list__frames');
+    const nextNumberFrame = frames.childNodes.length + 1;
+    const frame = App.createFrame(nextNumberFrame);
+
+    // const sourceCanvas = event.target.parentNode.firstChild;
+    const distinationCanvas = frame.firstChild;
+    const ctx = distinationCanvas.getContext('2d');
+
+    App.copyMainCanvas();
+
+    const canvas = document.querySelector('.canvas');
+    ctx.drawImage(canvas, 0, 0, 500, 500, 0, 0, 200, 200);
+
+    frames.append(frame);
+  }
+
   start() {
     const canvas = createElement('canvas', 'canvas', this.text);
-
     canvas.width = 500;
     canvas.height = 500;
 
@@ -16,15 +68,17 @@ export default class App {
 
     const label = createElement('label', 'label', 'Color: ', input);
 
-    const frameCanvas = createElement('canvas', 'frame__canvas');
-    frameCanvas.width = 200;
-    frameCanvas.height = 200;
-    const frame = createElement('li', 'frames__item', frameCanvas);
+    const frame = App.createFrame(1);
     const frames = createElement('ul', 'list__frames', frame);
 
-    document.body.append(label, frames, canvas);
+    const animation = createElement('canvas', 'canvas__animation');
+    animation.width = 300;
+    animation.height = 300;
+
+    document.body.append(label, frames, canvas, animation);
 
     App.draw();
+    setInterval(App.startAnimation, 1000);
   }
 
   static draw() {
@@ -69,11 +123,21 @@ export default class App {
   }
 
   static getFrame() {
-    const canvas = document.querySelector('.frame__canvas');
+    const canvas = document.querySelector('.canvas--active');
     const ctx = canvas.getContext('2d');
 
     const image = document.querySelector('.canvas');
 
     ctx.drawImage(image, 0, 0, 500, 500, 0, 0, 200, 200);
+  }
+
+  static startAnimation() {
+    const frames = [...document.querySelector('.list__frames').children];
+    const animation = document.querySelector('.canvas__animation');
+    const ctx = animation.getContext('2d');
+
+    frames.forEach((frame) => {
+      setTimeout(() => ctx.drawImage(frame.firstChild, 0, 0, 200, 200, 0, 0, 300, 300), 500);
+    });
   }
 }
